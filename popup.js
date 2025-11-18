6ea4f6a5232d233b6dedf1d4153d457cb43205b5
Леонид Черненко <nex-otaku@yandex.ru>
2025-11-18 08:32:44 +0300

    Progress dialog + main windows sync draft

--- popup.js
+++ popup.js
@@ -6,16 +6,49 @@
             return;
         }
 
+        let isProcessingNotes = false;
+
         sendButton.onclick = function (el) {
+            if (isProcessingNotes) {
+                // alert('We are already exporting!');
+
+                return;
+            }
+
+            isProcessingNotes = true;
+
             chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
                 chrome.scripting.executeScript(
                     {
                         target: {tabId: tabs[0].id},
-                        files: ['parser.js'],
+                        files: [
+                            'lib/jszip.js',
+                            'lib/FileSaver.js',
+                            'lib/convert.js',
+                            'lib/ProgressDialog.js',
+                            'parser.js'
+                        ],
                     },
                     () => {}
                 );
             })
         }
+
+        chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+            console.log('Background received:', request);
+
+            if (request.type === 'xiaomiNotesExporter') {
+
+                if (request.message === 'started') {
+                    sendButton.textContent = 'Exporting...';
+                }
+
+                if (request.message === 'finished') {
+                    sendButton.textContent = 'Download';
+
+                    isProcessingNotes = false;
+                }
+            }
+        });
     }
 )();
